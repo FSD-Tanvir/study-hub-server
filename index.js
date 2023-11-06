85c4112c478f5d98c00ce6bc823abd56e4bfe49a
tanvirfsd <tahasanpro@gmail.com>
2023-11-06 10:05:18 +0600

    create post method to insert data at AllAsssignment colletion

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 const uri = process.env.URI;
 
 //middleware
@@ -16,16 +16,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const assignmentCollection = client
+      .db("assignmentDB")
+      .collection("AllAssignments");
+
+    // app.get("/api/v1/all-assignments", async (req, res) => {
+    //   const cursor = assignmentCollection.find();
+    //   result = await cursor.toArray();
+    //   res.send(result);
+    // });
+
+    app.post("/api/v1/all-assignments", async(req, res)=>{
+        const assignment = req.body
+        console.log(req.body);
+        const result = await assignmentCollection.insertOne(assignment)
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -33,11 +53,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Welcome to Study Hub!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to Study Hub!");
+});
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
-})
+  console.log(`app listening on port ${port}`);
+});
